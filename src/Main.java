350dfb8819acc9ddf68478d16641e75d8d47c252
Anastasiyu <asy.safiulina@mail.ru>
2022-09-04 10:00:37 +0300

    ДЗ задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашнее задание");
+        task1();
+    }
+
+
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random randome = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+
+            arr[i] = randome.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+
+    public static void task1() {
+        int[] array = generateRandomArray();
+        int sum = 0;
+        for (int spending : array) {
+            sum += spending;
+        }
+        System.out.printf("Сумма трат за месяц составила %s рублей", sum);
+        int min = array[0];
+        int max = array[0];
+        for (int spending : array) {
+            if (min > spending) {
+                min = spending;
+            } else if (max < spending) {
+                max = spending;
+            }
+        }
+        System.out.println(" ");
+        {
+            System.out.printf("Минимальная сумма затрат за день составила %s рублей. \n" + "Максимальная суммазатрат за день составила %s рублей\n", min, max);
+
+        }
     }
 }
